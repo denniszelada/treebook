c18f90edabc7955409eb8ac31a295ee0754d0931
Dennis Zelada <denniszelada@gmail.com>
2014-04-03 23:14:28 +0000

    Friend and user errors resolved

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -2,7 +2,7 @@ class UserFriendshipsController < ApplicationController
   before_filter :authenticate_user!, only: [:new]
 
   def new
-    if params[:user_id]
+    if params[:friend_id]
       @friend = User.find(params[:friend_id])
       @user_friendship = UserFriendship.new(friend: @friend)
     else
